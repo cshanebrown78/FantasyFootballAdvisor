09547da62c6d85881c0412e2e5e759992c5258ad
cshanebrown78 <cshanebrown78@gmail.com>
2020-05-14 13:30:48 -0500

    initial draft.html coding added and more work on api gets for the page

--- public/js/draft_players.js
+++ public/js/draft_players.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var qbContainer = $(".qb-top-five");
+    var qbContainer = $(".qb-container");
     var qbList = $(".qbs")
     var nextRow = $(".next-qb-row")
     var qbs = [];
@@ -24,23 +24,48 @@ $(document).ready(function() {
         newTR.data("quarterback", qbData);
         newTR.append("<td>" + qbData.player_name + "</td>");
         newTR.append("<td>" + qbData.draft_rank + "</td>");
-        newTR.append("<td>" + qbData.team_name + "<td>");
+        newTR.append("<td>" + qbData.team_name + "</td>");
         newTR.append("<td><button class='user-team'>Draft</button></td>");
         newTR.append("<td><button class='other-team'>Drafted</button></td>")
+        newTR.append("</tr>")
         return newTR;
+        
     }
 
     function getQBs () {
-        $.get("api/players/position/QB", function(data) {
-            for(var j = 0; j < data.length; j++) {
-                var undrafted = [];
-                undrafted.push(data[i]);
-                for (var i = 0; i < 5; i++) {
-                    rowsToAdd.push(createNewRow(undrafted[i]))
-                }
-            }    
-            // renderQBList(rowsToAdd);
+        $.get("/api/players/position/QB", function(data) {
+            console.log(data);
+            
+                var rowsToAdd = [];
+                    for (var i = 0; i < 5; i++) {
+                        rowsToAdd.push(createNewRow(data[i]))
+                        console.log(rowsToAdd)
+                    }
+                qbContainer.append(rowsToAdd)
+                
+               
+            
         })
     }
+
+    // function getQBs () {
+    //     $.get("/api/players/position/QB", function(data) {
+    //         console.log(data);
+    //         for(var j = 0; j < data.length; j++) {
+    //             var undrafted = [];
+    //             if(data.user_team === 0 && data.other_team === 0) {
+    //             undrafted.push(data[j]);
+    //             }
+    //             var rowsToAdd = [];
+    //                 for (var i = 0; i < 5; i++) {
+    //                     rowsToAdd.push(createNewRow(undrafted[i]))
+    //                     console.log(undrafted)
+    //                 }
+    //             qbContainer.append(rowsToAdd)
+    //             console.log(rowsToAdd)
+    //         }    
+    //         console.log("Undrafted = " + undrafted)
+    //     })
+    // }
 
 })
