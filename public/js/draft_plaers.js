ae9bc826a02add0ff89d4ad839f7fa34430dba58
cshanebrown78 <cshanebrown78@gmail.com>
2020-05-13 19:58:32 -0500

    more work on the draft_players.js file

--- public/js/draft_plaers.js
+++ public/js/draft_plaers.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
-    var $qbContainer = $(".qb-top-five");
-    var $nextRow = $(".next-qb-row")
+    var qbContainer = $(".qb-top-five");
+    var qbList = $(".qbs")
+    var nextRow = $(".next-qb-row")
     var qbs = [];
 
     getQBs();
@@ -9,21 +10,36 @@ $(document).ready(function() {
     getWRs();
     getTEs();
 
-    function initializeRows () {
-        $qbContainer.empty();
-        var rowsToAdd = [];
-            for (var i = 0; i < 5; i++) {
-                rowsToAdd.push(createNewRow(qbs[i]))
-            }
-            $qbContainer.prepend(rowsToAdd)
-    }
+    // function initializeRows () {
+    //     qbContainer.empty();
+    //     var rowsToAdd = [];
+    //         for (var i = 0; i < 5; i++) {
+    //             rowsToAdd.push(createNewRow(qbs[i]))
+    //         }
+    //         qbContainer.prepend(rowsToAdd)
+    // }
 
-    
+    function createNewRow(qbData) {
+        var newTR = $("<tr>");
+        newTR.data("quarterback", qbData);
+        newTR.append("<td>" + qbData.player_name + "</td>");
+        newTR.append("<td>" + qbData.draft_rank + "</td>");
+        newTR.append("<td>" + qbData.team_name + "<td>");
+        newTR.append("<td><button class='user-team'>Draft</button></td>");
+        newTR.append("<td><button class='other-team'>Drafted</button></td>")
+        return newTR;
+    }
 
     function getQBs () {
         $.get("api/players/position/QB", function(data) {
-            qbs = data;
-            initializeRows()
+            for(var j = 0; j < data.length; j++) {
+                var undrafted = [];
+                undrafted.push(data[i]);
+                for (var i = 0; i < 5; i++) {
+                    rowsToAdd.push(createNewRow(undrafted[i]))
+                }
+            }    
+            // renderQBList(rowsToAdd);
         })
     }
 
