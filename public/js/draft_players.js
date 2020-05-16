4e13448d85bb7e8fd50a0762e7dbee1f5f35d82f
dcoleslaw86 <cole.desrosiers86@gmail.com>
2020-05-15 21:36:23 -0500

    Updated Draft, Clubhouse, Index, and CSS files for uniformity

--- public/js/draft_players.js
+++ public/js/draft_players.js
@@ -4,25 +4,28 @@ $(document).ready(function() {
     var rbContainer = $(".rb-container");
     var wrContainer = $(".wr-container");
     var teContainer = $(".te-container");
+    var userContainer = $(".user-container");
+    var otherContainer = $(".other-container");
     var qbList = $(".qbs")
     var nextRow = $(".next-qb-row")
     var qbs = [];
-    // $(document).on("click", "button.user-team", userDraft)
+
 
     getQBs();
     getRBs();
     getWRs();
     getTEs();
+    getUserTeam();
+    getOtherTeams();
 
 
     function createNewRow(positionData) {
         var newTR = $("<tr>");
-        // newTR.data("quarterback", qbData);
         newTR.append("<td>" + positionData.player_name + "</td>");
         newTR.append("<td>" + positionData.draft_rank + "</td>");
         newTR.append("<td>" + positionData.team_name + "</td>");
-        newTR.append("<td><button class='user-team'>Draft</button></td>");
-        newTR.append("<td><button class='other-team'>Drafted</button></td>")
+        newTR.append("<td><button class='user-team' player-id='" + positionData.id + "'>Draft</button></td>");
+        newTR.append("<td><button class='other-team'player-id='" + positionData.id + "'>Drafted</button></td>");
         newTR.append("</tr>")
         return newTR;
         
@@ -30,19 +33,15 @@ $(document).ready(function() {
 
     function getQBs () {
         $.get("/api/players/position/QB", function(data) {
-            // console.log(data);
-            // console.log(data[0].user_team);
             var undrafted = [];
             for (var j = 0; j < data.length; j++) {
                 if (data[j].user_team === false && data[j].other_team === false) {
                     undrafted.push(data[j])
                 }
             }
-            console.log(undrafted)
                 var rowsToAdd = [];
                     for (var i = 0; i < 5; i++) {
                         rowsToAdd.push(createNewRow(undrafted[i]))
-                        console.log(rowsToAdd)
                     }
                 qbContainer.append(rowsToAdd)
                 
@@ -53,19 +52,15 @@ $(document).ready(function() {
 
     function getRBs () {
         $.get("/api/players/position/RB", function(data) {
-            // console.log(data);
-            // console.log(data[0].user_team);
             var undrafted = [];
             for (var j = 0; j < data.length; j++) {
                 if (data[j].user_team === false && data[j].other_team === false) {
                     undrafted.push(data[j])
                 }
             }
-            console.log(undrafted)
                 var rowsToAdd = [];
                     for (var i = 0; i < 5; i++) {
                         rowsToAdd.push(createNewRow(undrafted[i]))
-                        console.log(rowsToAdd)
                     }
                 rbContainer.append(rowsToAdd)
         });
@@ -73,19 +68,15 @@ $(document).ready(function() {
 
     function getWRs () {
         $.get("/api/players/position/WR", function(data) {
-            // console.log(data);
-            // console.log(data[0].user_team);
             var undrafted = [];
             for (var j = 0; j < data.length; j++) {
                 if (data[j].user_team === false && data[j].other_team === false) {
                     undrafted.push(data[j])
                 }
             }
-            console.log(undrafted)
                 var rowsToAdd = [];
                     for (var i = 0; i < 5; i++) {
                         rowsToAdd.push(createNewRow(undrafted[i]))
-                        console.log(rowsToAdd)
                     }
                 wrContainer.append(rowsToAdd)
         });
@@ -93,26 +84,119 @@ $(document).ready(function() {
 
     function getTEs () {
         $.get("/api/players/position/TE", function(data) {
-            // console.log(data);
-            // console.log(data[0].user_team);
             var undrafted = [];
             for (var j = 0; j < data.length; j++) {
                 if (data[j].user_team === false && data[j].other_team === false) {
                     undrafted.push(data[j])
                 }
             }
-            console.log(undrafted)
                 var rowsToAdd = [];
                     for (var i = 0; i < 5; i++) {
                         rowsToAdd.push(createNewRow(undrafted[i]))
-                        console.log(rowsToAdd)
                     }
                 teContainer.append(rowsToAdd)
         });
     };
 
-    // function userDraft() {
-    //     var userTeam = 
-    // }
+
+    $(document).on("click", "button.user-team", function (event) {
+        event.preventDefault();
+        var userTeamId = $(this).attr("player-id");
+        var isUserDrafted = {
+            user_team: 1
+        };
+        $.ajax("api/players/" + userTeamId, {
+            type: "PUT",
+            data: isUserDrafted,
+        }).then(function() {
+            location.reload();
+        });
+    });
+
+    $(document).on("click", "button.other-team", function (event) {
+        event.preventDefault();
+        var otherTeamId = $(this).attr("player-id");
+        var isOtherDrafted = {
+            other_team: 1
+        };
+        $.ajax("api/players/" + otherTeamId, {
+            type: "PUT",
+            data: isOtherDrafted,
+        }).then(function() {
+            location.reload();
+        });
+    });
+
+    function createNewUserTeamRow(positionData) {
+        var newTR = $("<tr>");
+        newTR.append("<td>" + positionData.player_name + "</td>");
+        newTR.append("<td>" + positionData.draft_rank + "</td>");
+        newTR.append("<td>" + positionData.team_name + "</td>");
+        newTR.append("<td>" + positionData.position + "</td>");
+        newTR.append("<td>" + positionData.bye + "</td>");
+        newTR.append("</tr>")
+        return newTR;
+        
+    }
+
+    function getUserTeam () {
+        $.get("/api/players/", function(data) {
+            var userDrafted = [];
+            for (var j = 0; j < data.length; j++) {
+                if (data[j].user_team === true) {
+                    userDrafted.push(data[j])
+                }
+            }
+                var rowsToAdd = [];
+                    for (var i = 0; i < userDrafted.length; i++) {
+                        rowsToAdd.push(createNewUserTeamRow(userDrafted[i]))
+                    }
+                userContainer.append(rowsToAdd)
+                
+               
+            
+        });
+    };
+
+    function getOtherTeams () {
+        $.get("/api/players/", function(data) {
+            var otherDrafted = [];
+            for (var j = 0; j < data.length; j++) {
+                if (data[j].other_team === true) {
+                    otherDrafted.push(data[j])
+                }
+            }
+                var rowsToAdd = [];
+                    for (var i = 0; i < otherDrafted.length; i++) {
+                        rowsToAdd.push(createNewUserTeamRow(otherDrafted[i]))
+                    }
+                otherContainer.append(rowsToAdd)
+                
+               
+            
+        });
+    };
+
+    $(document).on("click", "button.undo-draft", function (event) {
+        event.preventDefault();
+        if (confirm("Are you sure you want to reset the draft?")) {
+            var undoDraft = {
+                user_team: 0,
+                other_team: 0
+            };
+            for(var x = 0; x < 100; x++) {
+                $.ajax("api/players/" + x, {
+                    type: "PUT",
+                    data: undoDraft,
+                }).then(function() {
+                    location.reload();
+                });
+            }
+        } else {
+            location.reload();
+        }
+    });
+
+
 
 })
