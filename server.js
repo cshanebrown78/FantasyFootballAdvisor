cd256e3de4ee89980df8771326be92a9431a9f33
cshanebrown78 <cshanebrown78@gmail.com>
2020-05-16 11:32:02 -0500

    more travis work

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-// Test
+// Tests
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
